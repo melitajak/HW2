02ee14a6ca371ad4ef25e71294a4d812f01f9083
melitajak <127343818+melitajak@users.noreply.github.com>
2023-05-31 21:05:18 +0300

    added trees comments and identifying simmilar genomes

--- analysis_annotation.sh
+++ analysis_annotation.sh
@@ -10,62 +10,39 @@
 #BUSCOs are complete and single-copy, indicating good quality and representation of essential genes. However, there are a 
 #few fragmented and missing BUSCOs, suggesting potential gaps or errors in the assemblies. 
 
-#BLAST
+#BLAST 
+#nucleo
 makeblastdb -in contigs.fasta -dbtype nucl -parse_seqids
-blastn -db contigs.fasta -query CP015498.fasta > blast.rez
+blastn -db contigs.fasta -query CP015498nucl.fasta > blast_nucl.rez
+#protein
+makeblastdb -in ERRcontigs.fasta -dbtype prot -parse_seqids
+tblastn -db ERRcontigs.fasta -query CP015498prot.fasta -outfmt 6 -out ERRoutput.txt > blast_prot.rez
 
 #Comparing and describing gene predictions:
 #ERR204044 assembly:
-#Number of predicted genes: RAST- 2564, GeneMarks- 2311 and BLAST- 1813
+#Number of predicted genes: RAST- 2564, GeneMarks- 2311 and BLAST nucl- 1813, prot- 2091
 #SRR15131330 assembly:
-#Number of predicted genes: RAST- 2772, GeneMarks- 2550 and BLAST- 1909
+#Number of predicted genes: RAST- 2772, GeneMarks- 2550 and BLAST nucl- 1909, prot-2095
 #SRR18214264 assembly:
-#Number of predicted genes: RAST- 2521, GeneMarks- 2338 and BLAST- 1811
+#Number of predicted genes: RAST- 2521, GeneMarks- 2338 and BLAST nucl- 1811, prot- 2089
 #
 #Predictions created by BLAST had the smallest amount of predicted genes, and RAST had the biggest. Also, number of predicted
 #genes, are very similar in ERR204044 and SRR18214264 assemblies, accross all the different tools that were used to make 
-#predictions.
+#predictions. 
 
 #Create a phylogenetic tree from 16S sequences
+#aligning files
 muscle -in 16s.fasta -out 16s.aln
 
-+-----------------------------------------------------------Staphylococcus_16S
-|
-|  +**16S_ERR204044_NODE_122_107-1667
-+--|
-|  +**16S_SRR18214264_NODE_123_107-1667
-|
-|     +--16S_SRR15131330_NODE_139_4-956
-|  +**|
-|  |  |  +**16S_CP015498.1_94173-95733
-|  |  +**|
-|  |     +**16S_CP015498.1_474525-476085
-+--|
-   +**16S_SRR15131330_NODE_148_0-603
-
 #Multi-gene tree
+#aligning files
 muscle -in multi.fasta -out multi.aln
 
-+--Staphylococcus_16S
-|
-|     +**16S_ERR204044_NODE_122_107-1667
-|  +**|
-|  |  +**16S_SRR18214264_NODE_123_107-1667
-+--|
-|  |                                      +**Glucokinase
-|  |                           +----------|
-|  |                           |          +-----------------Protein_translocase_subunit_SecA
-|  |              +------------|
-|  |              |            +--------Penicillin-binding_protein
-|  |           +--|
-|  |           |  +-----------------Ribonuclease_III
-|  +-----------|
-|              +-------------Histidine_tRNA_ligase
-|
-|        +--16S_SRR15131330_NODE_139_4-956
-|     +**|
-|     |  +**16S_CP015498.1_94173-95733
-|  +**|
-|  |  +**16S_CP015498.1_474525-476085
-+--|
-   +**16S_SRR15131330_NODE_148_0-603
+#Comparing trees:
+#From the trees we can see same tendency that ERR204044 with SRR18214264 and SRR15131330 with CP015498 always end up in the same 
+#clusters. Also we can see that Staphylococus is usually not in in the same clusters with assemblies or reference.
+
+#Using all data, identify if any of genomes are more similar to each other than to the third one (or reference genome):
+#Genomes ERR204044 and SRR18214264 are more simmilar to each other, we can see it in the phylogenetic trees and also
+#they have very simmilar predicted genes count, as well as closer coverage than SRR15131330 sample. Closest to the reference genome
+#is SRR15131330, as we can see it in the phylogenetic trees.
